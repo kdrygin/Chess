3896cbbd5107ac960cac039ef1007f6326abaeff
Дрыгин Константин <kdrygin@gmail.com>
2021-06-01 21:28:33 -0700

    Piece move and setup handled to Field_Click

--- WPFSimpleChess/MainWindow.xaml.cs
+++ WPFSimpleChess/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessCore;
 
 namespace WPFSimpleChess
 {
@@ -20,9 +21,46 @@ namespace WPFSimpleChess
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Piece piece;
+        private Button piecePosition;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Field_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            int row = Grid.GetRow(clickedButton);
+            int col = Grid.GetColumn(clickedButton);
+
+            //clear
+            if (clickedButton.Content != null && piece != null)
+            {
+                clickedButton.Content = null;
+                piece = null;
+                clickedButton = null;
+                return;
+            }
+
+            // set piece
+            if (clickedButton.Content == null && piece == null)
+            {
+                var selPieceName = ((ListBoxItem)lbPieces.SelectedValue).Content.ToString();
+                clickedButton.Content = selPieceName;
+                piece = PieceMaker.Make(selPieceName, col, row);
+                piecePosition = clickedButton;
+                return;
+            }
+
+            // move
+            if (clickedButton.Content == null && piece.Move(col, row))
+            {
+                clickedButton.Content = piecePosition.Content.ToString();
+                piecePosition.Content = null;
+                piecePosition = clickedButton;
+            }
         }
     }
 }
